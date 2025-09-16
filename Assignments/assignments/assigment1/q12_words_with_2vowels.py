8ae51fbb5960c3c64c417f1c07e7dd78753c5fa6
SatyamShivamCDAC <satyamshivamcdac@gmail.com>
2025-09-16 13:46:30 +0530

    Assignment 1

---
+++ Assignments/assignments/assigment1/q12_words_with_2vowels.py
@@ -0,0 +1,19 @@
+#list of word with 2 or more vowels
+
+texts = ['Apple', 'Elephant', 'Mango', 'Orange', 'Cat']
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+
+texts_vowels = []
+
+for word in texts:
+    vowelCount = 0
+    for char in word:
+        if char.lower() in vowels:
+            vowelCount += 1
+
+    if vowelCount >= 2:
+        texts_vowels.append(word)
+
+
+print(texts_vowels)
---
+++ Assignments/assignments/assigment1/q8_add_even_till_100.py
@@ -0,0 +1,6 @@
+# 8. Adding even element in list from 1 to 100
+even_list = []
+for i in range(1,101):
+    if i%2==0:
+        even_list.append(i)
+print(even_list)
